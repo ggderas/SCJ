6ff9e8f8108fcd781f2f40eeb6e39551b3378ae8
ClaudioPaz <ClaudioPaz@MacBook-Pro-de-Claudio.local>
2015-07-06 19:06:44 -0600

    Ultimas Actualizaciones

--- pages/CargaAcademica/RegistroDocentes/RegistrarDocente.php
+++ pages/CargaAcademica/RegistroDocentes/RegistrarDocente.php
@@ -1,38 +1,49 @@
 
-<?php  
-function limpiar($tags)
-{
-    $tags = strip_tags($tags);
-    return $tags;
-    
-    
-}
+<?php
+
+
+include("../../../conexion/conn.php");  
+
+
+   $maindir = "../../../";
+
+  require_once($maindir."funciones/check_session.php");
+
+  require_once($maindir."funciones/timeout.php");
+  
+   if(!isset( $_SESSION['user_id'] ))
+  {
+    header('Location: '.$maindir.'login/logout.php?code=100');
+    exit();
+  }
+
+
 	try{
 
-		$identi = limpiar($_POST['identidad']);
-    	$pNombre = limpiar($_POST['primerNombre']);
-    	$sNombre = limpiar($_POST['segundoNombre']);
-	    $pApellido = limpiar($_POST['primerApellido']);
-	    $sApellido = limpiar($_POST['segundoApellido']);
-	    $fNac = limpiar($_POST['fecha']);
+		  $identi = $_POST['identidad'];
+    	$pNombre = $_POST['primerNombre'];
+    	$sNombre = $_POST['segundoNombre'];
+	    $pApellido = $_POST['primerApellido'];
+	    $sApellido = $_POST['segundoApellido'];
+	    $fNac = $_POST['fecha'];
 	    $sexo = $_POST['sexo'];
-	    $tel = limpiar($_POST['telefono']);
-	    $direc = limpiar($_POST['ciudadNatal']);
-	    $email = limpiar($_POST['correo']);
+	    $tel = $_POST['telefono'];
+	    $direc = $_POST['ciudadNatal'];
+	    $email = $_POST['email'];
 	    $estCivil = $_POST['estCivil'];
 	    $nacionalidad = $_POST['nacionalidad'];
-
+      
 		      // realizamos la consulta
-      $query = ""; // Lugar para el pl
+      $query = "SP_REGISTRAR_DOCENTE( , @pcMensajeError)"; // Lugar para el pl
       $result = mysql_query($query, $conexion) or die("error en la consulta"); // hacemos la consulta a la base de datos
-      $mensaje = "El usuario se ha creado exitosamente...";
+      $mensaje = " ".$pNombre . " " . $pApellido . " ha sido agregado(a) con éxito!";
       $codMensaje = 1;
     
     }catch(PDOExecption $e){
-      $mensaje="No se ha procesado su peticion, comuniquese con el administrador del sistema";
-      $codMensaje =0;
+      $mensaje = 'error al ingresar el registro o registro actualmente existente';
+      $codMensaje = 0;
     }
-
+    
   if(isset($codMensaje) and isset($mensaje)){
     if($codMensaje == 1){
       echo '<div class="alert alert-success alert-succes">
@@ -43,5 +54,5 @@ function limpiar($tags)
         <a href="#" class="close" data-dismiss="alert">&times;</a>
         <strong> Error! </strong>'.$mensaje.'</div>';
     }
-  } 
+  }
  ?>
